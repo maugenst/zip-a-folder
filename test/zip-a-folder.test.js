e28e5834ae1dca9ce0fe67a74b7281bc088eaa54
Marius Augenstein <marius.augenstein@sap.com>
2020-02-06 10:42:09 +0100

    Corrects same folder error

--- test/zip-a-folder.test.js
+++ test/zip-a-folder.test.js
@@ -1,56 +1,63 @@
-"use strict";
+'use strict';
 
-const fs = require("fs");
-const path = require("path");
-const zipafolder = require("../lib/zip-a-folder");
+const fs = require('fs');
+const path = require('path');
+const zipafolder = require('../lib/zip-a-folder');
 
-describe("Zip-A-Folder Test", function() {
-  let testasync = path.resolve(__dirname, "testasync.zip");
-  let testcallback = path.resolve(__dirname, "testcallback.zip");
-  let testnotexisting = path.resolve(__dirname, "/notexisting/testcallback.zip");
+describe('Zip-A-Folder Test', function() {
+    let testasync = path.resolve(__dirname, 'testasync.zip');
+    let testasyncSameDirectory = path.resolve(__dirname, 'data/testasync.zip');
+    let testcallback = path.resolve(__dirname, 'testcallback.zip');
+    let testnotexisting = path.resolve(__dirname, '/notexisting/testcallback.zip');
 
-  beforeAll(() => {
-    if (fs.existsSync(testasync)) {
-      fs.unlinkSync(testasync);
-    }
-    if (fs.existsSync(testcallback)) {
-      fs.unlinkSync(testcallback);
-    }
-  });
+    beforeAll(() => {
+        if (fs.existsSync(testasync)) {
+            fs.unlinkSync(testasync);
+        }
+        if (fs.existsSync(testasyncSameDirectory)) {
+            fs.unlinkSync(testasyncSameDirectory);
+        }
+        if (fs.existsSync(testcallback)) {
+            fs.unlinkSync(testcallback);
+        }
+    });
 
-  it("ASYNC: Zip test folder", async function() {
-    expect.assertions(1);
-    await zipafolder.zip(path.resolve(__dirname, "data/"), testasync);
+    it('ASYNC: Zip test folder and zip target in same directory should throw an error', async () => {
+        expect.assertions(1);
+        await expect(zipafolder.zip(path.resolve(__dirname, 'data/'), testasyncSameDirectory)).rejects.toThrow(
+            /Source and target folder must be different./
+        );
+    });
 
-    expect(fs.existsSync(testasync)).toBe(true);
-  });
+    it('ASYNC: Zip test folder', async () => {
+        expect.assertions(1);
+        await zipafolder.zip(path.resolve(__dirname, 'data/'), testasync);
 
-  it("ASYNC: Zip test folder failing", async function() {
-    expect.assertions(1);
-    try {
-      await zipafolder.zip(path.resolve(__dirname, "notexisting/"), testasync);
-    } catch (e) {
-      expect(e.message).toMatch(/no such file or directory/);
-    }
-  });
+        expect(fs.existsSync(testasync)).toBe(true);
+    });
 
-  it("ASYNC: Zip test folder into a zipfile in a notexisting folder", async function() {
-    expect.assertions(1);
+    it('ASYNC: Zip test folder failing', async () => {
+        expect.assertions(1);
+        try {
+            await zipafolder.zip(path.resolve(__dirname, 'notexisting/'), testasync);
+        } catch (e) {
+            expect(e.message).toMatch(/no such file or directory/);
+        }
+    });
 
-    try {
-      await zipafolder.zip(path.resolve(__dirname, "data/"), testnotexisting);
-    } catch (e) {
-      expect(e.message).toMatch(/no such file or directory/);
-    }
-  });
+    it('ASYNC: Zip test folder into a zipfile in a notexisting folder', async () => {
+        expect.assertions(1);
 
-  it("CALLBACK: Zip test folder", function() {
-    zipafolder.zipFolder(
-      path.resolve(__dirname, "data/"),
-      testcallback,
-      () => {
-        expect(fs.existsSync(testcallback)).toBe(true);
-      }
-    );
-  });
+        try {
+            await zipafolder.zip(path.resolve(__dirname, 'data/'), testnotexisting);
+        } catch (e) {
+            expect(e.message).toMatch(/no such file or directory/);
+        }
+    });
+
+    it('CALLBACK: Zip test folder', () => {
+        zipafolder.zipFolder(path.resolve(__dirname, 'data/'), testcallback, () => {
+            expect(fs.existsSync(testcallback)).toBe(true);
+        });
+    });
 });
