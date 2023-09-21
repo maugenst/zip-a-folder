19e108fa242cb5a51d873cf528fa6a3d5fb081d4
Marius Augenstein <marius.augenstein@sap.com>
2023-09-21 16:42:20 +0200

    Created missing declaration file bump version to 3.1.3

---
+++ dist/ZipAFolder.d.ts
@@ -0,0 +1,19 @@
+/// <reference types="node" />
+import { WriteStream } from 'fs';
+export declare enum COMPRESSION_LEVEL {
+    uncompressed = 0,
+    medium = 5,
+    high = 9
+}
+export type ZipAFolderOptions = {
+    compression?: COMPRESSION_LEVEL;
+    customWriteStream?: WriteStream;
+    destPath?: string;
+};
+export declare class ZipAFolder {
+    static tar(src: string, tarFilePath: string | undefined, zipAFolderOptions?: ZipAFolderOptions): Promise<void | Error>;
+    static zip(src: string, zipFilePath: string | undefined, zipAFolderOptions?: ZipAFolderOptions): Promise<void | Error>;
+    private static compress;
+}
+export declare const zip: typeof ZipAFolder.zip;
+export declare const tar: typeof ZipAFolder.tar;
